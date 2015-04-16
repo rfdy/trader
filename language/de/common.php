c624d1b06b7a38b64cfef4cd7695b2acac275840
Raphaël M. <Galixte@users.noreply.github.com>
2015-04-16 15:09:53 +0200

    Update of GERMAN translation for add one more language key
    update for common.php file

--- language/de/common.php
+++ language/de/common.php
@@ -1,19 +1,41 @@
 <?php
 /**
- *
- * @package phpBB Extension - Trader [German]
- * @copyright (c) 2013 phpBB Group
- * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
- *
- */
+*
+* @package phpBB Extension - Trader [German]
+* @copyright (c) 2015 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
 if (!defined('IN_PHPBB'))
 {
-    exit;
+	exit;
 }
+
 if (empty($lang) || !is_array($lang))
 {
-    $lang = array();
+	$lang = array();
 }
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, array(
     'ALREADY_GIVEN_FEEDBACK'    =>  'Du hast diesen User bereits für diesen Thread bewertet.',
     'ACL_CAT_TRADER'            =>  'Trader',
@@ -118,4 +140,5 @@ $lang = array_merge($lang, array(
     'RECIPIENT'				    => 'Recipient',
     'REQUIRED_CHARACTERS'		=> '* Required 10-200 Characters',
     'REQUIRED_INTENTION_TO'		=> '* Please select whether your intention is to Buy, Sell or Trade.',
+    'FROM_TRADER'		=> 'From',
 ));
